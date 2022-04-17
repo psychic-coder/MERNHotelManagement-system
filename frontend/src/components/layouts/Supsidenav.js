9be220edbc77ac17afe5645fd1df9ac2fa39ccc6
Samudika0000 <aruni.samu1@gmail.com>
2022-04-17 13:04:02 +0530

    modified navbar.css in the frontend

--- frontend/src/components/layouts/Supsidenav.js
+++ frontend/src/components/layouts/Supsidenav.js
@@ -10,9 +10,10 @@ export default function Supsidenav() {
         <div>
 
             <div class="sidebar">
-            
+
                 <Link to="/supmanager/view">View Suppliers</Link>
                 <Link to="/supmanager/add">Add Supplier</Link>
+                <Link to="/supmanager/add">Supplier History</Link>
             </div>
 
 
