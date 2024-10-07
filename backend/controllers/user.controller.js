3d03edfd47f97b3c6a030174d3acfdb92d446e9d
psychic-coder <rohitganguly450@gmail.com>
2024-10-07 20:38:55 +0530

    trying to correct the pgsql

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -8,34 +8,33 @@ import { cookieOptions } from "../utils/features.js";
 export const newUser = TryCatch(async (req, res, next) => {
     const { username, password, email, phone, address } = req.body;
 
-   
-    const user = await pool.query('SELECT * FROM People WHERE username=$1 OR email=$2;', [username, email]);
-    if (user.rowCount !== 0) {
+    
+    const user = await pool.query( 'SELECT "username", "email", "phone", "address" FROM "People" WHERE "username" = $1 OR "email" = $2', [username, email]);
+    if (user.rowCount != 0) {
         return res.status(404).json({ success: false, message: 'User already exists with this email and Username' });
     }
 
    
-    if (!username || !email || !password) {
+    if (!username || !email || !password || !phone || !address) {
         return next(new ErrorHandler(404, "All fields are required"));
     }
 
-    
+   
     const salt = bcryptjs.genSaltSync(10);
     const hashedPassword = bcryptjs.hashSync(password, salt);
 
    
     const query = `
-      INSERT INTO "User" (username, password, email, phone, address)
-      VALUES ($1, $2, $3, $4, $5)
-      RETURNING *;
+      INSERT INTO "People" (username, password, email, phone, address)
+      VALUES ($1, $2, $3, $4, $5);
     `;
     const values = [username, hashedPassword, email, phone, address];
     const userCreated = await pool.query(query, values);
 
-  
+    
     const token = jwt.sign({ _id: userCreated.rows[0].username }, process.env.JWT_SECRET);
 
-   
+    // Send response with cookie
     const message = "User created successfully";
     return res
         .status(201)
